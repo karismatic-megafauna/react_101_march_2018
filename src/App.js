80bdc6262c54826b9bb38a3fc66276115e2e644f
Thomas O'Boyle <thomas.oboyle@procore.com>
2018-03-29 09:57:04 -0700

    Introduce filtering by name

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Page, Header, Box, Flex } from "@procore/core-react";
+import { Page, Header, Box, Flex, Input } from "@procore/core-react";
 import Sidebar from "./components/Sidebar";
 import PokeCard from "./components/PokeCard";
 import Pokedex from "pokemon-metadata";
 
 class App extends Component {
-  // ------ this.buildPokeCards -------
-  // Instance Method
-  // Receives an array of pokemon names,
-  // Returns an array of PokeCards, one for each pokemon name supplied
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
   buildPokeCards = pokemonNames => {
     return pokemonNames.map(pokemonName => {
       const pokemon = Pokedex[pokemonName];
@@ -17,19 +27,20 @@ class App extends Component {
     });
   };
 
-  // ------ this.sortedAndFilteredPokemon -------
-  // Instance Method
-  // Receives no arguments
-  // Returns an array of pokemon, sorted and filtered using component's state
+  getPokemonNames = () => {
+    const pokemonName = Object.keys(Pokedex);
+    return pokemonName;
+  };
+
   sortAndFilterPokemon = () => {
-    // TODO: sort/filter the Pokedex based on user input
-    return Pokedex;
+    const pokemonName = this.getPokemonNames();
+    const result = pokemonName.filter( item => item.includes(this.state.value) );
+    return result;
   };
 
   // TODO: Create the Sort/Filter controls
   render() {
-    const pokemon = this.sortAndFilterPokemon();
-    const pokemonNames = Object.keys(pokemon);
+    const pokemonNames = this.sortAndFilterPokemon();
     const pokeCards = this.buildPokeCards(pokemonNames);
 
     return (
@@ -39,7 +50,7 @@ class App extends Component {
             <Header.H1>Choose your Pokemon</Header.H1>
           </Page.ToolHeader>
           <Page.Filters>
-            <Box padding="sm">Sort by stat, filter by type...</Box>
+            <Box padding="sm"><Input value={this.state.value} onChange={this.handleChange}></Input></Box>
           </Page.Filters>
           <Page.Body>
             <Flex wrap="wrap">{pokeCards}</Flex>
